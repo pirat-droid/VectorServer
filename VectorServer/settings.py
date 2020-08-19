8f944ba33199c8f5590c23346b1ed8127ba22079
pirat-droid <aldegra80@gmail.com>
2020-08-19 23:42:00 +0500

    Исправление шаблона

--- VectorServer/settings.py
+++ VectorServer/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'umg51lbrvfr9ws6!g!!-z7htw$%s2$ve&eg9l4+#8yh+k2n0i7'
+SECRET_KEY = 'umg51lbrvfr9ws6!g!!-z7htw$%s2$ve&eg9l4+#8yh+k2n0i'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
